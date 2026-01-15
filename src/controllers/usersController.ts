e5874663d55f84beb8763d73d8e5b52ca7feeb93
ifoca <foca.ionut@gmail.com>
2026-01-15 13:56:39 +0100

    add models

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,7 +1,12 @@
 import type { RequestHandler } from 'express';
+import { Users } from '#models';
 
-export const getUsers: RequestHandler = (req, res) => {
-  console.log('GET users');
+export const getUsers: RequestHandler = async (req, res) => {
+  const users = await Users.find();
+  if (!users) {
+    throw new Error('Something went wrong, could not get users', { cause: 404 });
+  }
+  return res.status(200).json(users);
 };
 
 export const createUsers: RequestHandler = (req, res) => {
